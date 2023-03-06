17415344c9f33c5d7feecab22ff680709b92d5b0
fc <fc@gsmk.de>
2023-03-06 09:24:21 +0100

    add partytown dependency

--- src/root.tsx
+++ src/root.tsx
@@ -1,28 +1,40 @@
 import { component$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import {
+	QwikCityProvider,
+	RouterOutlet,
+	ServiceWorkerRegister,
+} from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
+import 'uno.css';
+import { QwikPartytown } from './components/partytown/partytown';
 
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Dont remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
-      </body>
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<head>
+				<meta charSet="utf-8" />
+				<QwikPartytown forward={['dataLayer.push']} />
+				<script
+					async
+					type="text/partytown"
+					src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+				/>
+				<link rel="manifest" href="/manifest.json" />
+				<RouterHead />
+			</head>
+			<body lang="en">
+				<RouterOutlet />
+				<ServiceWorkerRegister />
+			</body>
+		</QwikCityProvider>
+	);
 });
